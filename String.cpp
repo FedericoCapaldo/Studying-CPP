1304077cbe73b7b82ca1e7710123b8518b4f1a75
FedericoKCL <fed.deployment@aol.co.uk>
2016-10-17 16:56:09 -0700

    added correction for strcat

--- String.cpp
+++ String.cpp
@@ -20,11 +20,14 @@ char * String::strcpy( char *dest, const char *src ) {
 // concatenates two strings
 char * String::strcat(char *dest, const char *src) {
   int i, j;
-  for (i = 0; dest[i] != '\0'; i++) {
-    for (j = 0; src[j] != '\0'; j++) {
-      dest[i+j] = src[j];
-    }
+  for (i = 0; dest[i] != '\0'; i++) ;
+  for (j = 0; src[j] != '\0'; j++) {
+    dest[i+j] = src[j];
   }
   dest[i+j] = '\0';
   return dest;
+}
+
+int String::strcmp( const char *left, const char *right ) {
+
 }
--- string_test.cpp
+++ string_test.cpp
@@ -5,10 +5,9 @@ using namespace std;
 
 char * myStrcat(char *dest, const char *src) {
   int i, j;
-  for (i = 0; dest[i] != '\0'; i++) {
-    for (j = 0; src[j] != '\0'; j++) {
-      dest[i+j] = src[j];
-    }
+  for (i = 0; dest[i] != '\0'; i++) ;
+  for (j = 0; src[j] != '\0'; j++) {
+    dest[i+j] = src[j];
   }
   dest[i+j] = '\0';
   return dest;
@@ -18,10 +17,11 @@ char * myStrcat(char *dest, const char *src) {
 int main() {
   char str[80];
   strcpy (str,"these ");
-  strcat (str,"strings ");
-  strcat (str,"are ");
-  strcat (str,"concatenated.");
-  puts (str);
+  myStrcat (str,"strings ");
+  myStrcat (str,"are ");
+  myStrcat (str,"concatenated.");
   cout << String::strlen(str) << endl;
+  puts (str);
+
   return 0;
 }
